158cbbc949bdfbbc1012455a8205eada27a477f3
jacksonyuan256 <e0957476@u.nus.edu>
2023-11-13 15:38:50 +0800

    Bug fix for search

--- src/main/java/seedu/address/logic/commands/SearchCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchCommand.java
@@ -48,7 +48,7 @@ public class SearchCommand extends Command {
         tagsToSearch = tags;
     }
 
-    private Predicate<Person> getPersonPredicateFromModule(Model model) {
+    private Predicate<Person> getPersonPredicateFromModule() {
         if (personNameToSearch.isEmpty()
                 && modulesToSearch.isEmpty()
                 && tutorialsToSearch.isEmpty()
@@ -84,7 +84,7 @@ public class SearchCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(getPersonPredicateFromModule(model));
+        model.updateFilteredPersonList(getPersonPredicateFromModule());
         return new CommandResult(MESSAGE_SUCCESS);
     }
     @Override
